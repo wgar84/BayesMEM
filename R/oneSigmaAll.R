37b6f063c8b28801edc6a7124fb7e10995480ce8
Guilherme Garcia <wgar@usp.br>
2014-09-04 20:49:32 -0300

    minor

--- R/oneSigmaAll.R
+++ R/oneSigmaAll.R
@@ -6,8 +6,8 @@ require (plyr)
 require (plotrix)
 require (doMC)
 #registerDoMC (cores = 3)
-#registerDoMC (cores = 8)
-registerDoMC (cores = 60)
+registerDoMC (cores = 8)
+#registerDoMC (cores = 60)
 require (reshape2)
 require (ggplot2)
 require (rstan)
@@ -30,22 +30,5 @@ All $ fit.SA <- mainModel (110, OneDef, Tree [[1]], 'local',
                            model = 'oneSigma_Anc',
                            pars = c('Xbar', 'alpha', 'Sigma', 'Sigma_bm'),
                            warmup = 50, iter = 100)
-
-All $ oneSigma <- list()
-All $ oneSigma <-
-  within (All $ oneSigma,
-          {
-            k <- 39
-            m <- 109
-            C <- vcvPhylo (Tree [[1]], anc.nodes = TRUE)
-            ni <- Aux $ sample.size 
-            ni_max <- max (ni)
-            X <- array (0, c (m, ni_max, k))
-            for (i in 1:m)
-              X [i, 1:ni [i], ] <-
-                OneDef [[i]] $ local
-            priorS <- post.vcv $ ss.grand.mean
-            priorX <- OneDef [['Cebus_apella']] $ mean
-          })
 
 save (All, file = 'All.RData')
