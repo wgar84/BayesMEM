05a781d901536ec0ac8f2dd8ea2388a39b412cf6
Guilherme Garcia <wgar@usp.br>
2014-10-31 22:39:35 -0200

    minor

---
+++ R/diagHomi.R
@@ -0,0 +1,271 @@
+require (ape)
+require (Morphometrics)
+require (expm)
+require (plyr)
+require (plotrix)
+require (doMC)
+registerDoMC (cores = 4)
+#registerDoMC (cores = 8)
+#registerDoMC (cores = 60)
+require (reshape2)
+require (ggplot2)
+require (gridBase)
+require (gridExtra)
+require (grid)
+require (rstan)
+require (phytools)
+require (geiger)
+require (mvtnorm)
+
+attach ('../../Databases/Reference.RData')
+attach ('../../Databases/OneDef/ED.RData')
+attach ('../../Databases/OneDef/OneDef.RData')
+attach ('../../Databases/Tree.RData')
+attach ('../../Databases/Aux.RData')
+attach ('../../covTensor/Work/post.vcv.RData')
+
+.source.files <- dir('../FuncR/', pattern = '.R', full.names = TRUE)
+.source.files <- .source.files [!grepl ('~', .source.files)]
+for (i in 1:length (.source.files))
+  source (.source.files [i])
+
+load ('testRuncieHomi.RData')
+
+system ('mkdir Homi')
+folder <- 'Homi/'
+
+runciePost <- list()
+runciePost $ ext <- llply (runcie.test, extract)
+
+runciePost $ subtree <- extract.clade (Tree [[5]], 152)
+
+runciePost $ ext <-
+  llply (runciePost $ ext, nameStanExt,
+         names.list = list ('trait' = colnames (OneDef [[1]] $ local),
+           'node' = runciePost $ subtree $ tip.label,
+           'node' = as.character (8:11),
+           'factor' = paste ('lambda', 1:6, sep = ''),
+           'iterations' = 1:100))
+
+str (runciePost $ ext [[1]])
+
+runciePost $ combine <- sflist2stanfit(runcie.test)
+
+runciePost $ combine.ext <- extract(runciePost $ combine)
+
+runciePost $ combine.ext <-
+  nameStanExt (runciePost $ combine.ext, 
+               names.list = list ('trait' = colnames (OneDef [[1]] $ local),
+                 'node' = runciePost $ subtree $ tip.label,
+                 'node' = as.character (8:11),
+                 'factor' = paste ('lambda', 1:6, sep = ''),
+                 'iterations' = 1:400))
+
+
+runciePost $ gelman.plot <- DiagGelman(runciePost $ ext)
+
+runciePost $ gelman.plot.comp <-
+  do.call (arrangeGrob, c(runciePost$gelman.plot, 'ncol' = 3))
+ggsave(paste (folder, 'runcie.gelman.pdf', sep = ''),
+       runciePost $ gelman.plot.comp, width = 24, height = 8)
+              
+runciePost $ trace <-
+  DiagTrace (runciePost $ combine.ext)
+runciePost $ trace.comp <-
+  do.call (arrangeGrob, c(runciePost $ trace, 'nrow' = 3))
+ggsave(paste (folder, 'runcie.trace.pdf', sep = ''),
+       runciePost $ trace.comp, width = 24, height = 24)
+
+runciePost $ response <- llply (runciePost $ ext, PostDeltaZ,
+                                tree = runciePost $ subtree,
+                                beta = TRUE)
+
+runciePost $ combine.response <-
+  PostDeltaZ(runciePost $ combine.ext, runciePost $ subtree, TRUE)
+
+pdf(file = paste (folder, 'runcie.beta.pdf', sep = ''), width = 16, height = 8)
+llply(runciePost $ response, DiagBeta, runciePost $ subtree, slice = 'logCS')
+dev.off (dev.cur ())
+
+runciePost $ diagW <-
+  llply (runciePost $ ext, DiagW,
+         vcv.terminal.list = llply (OneDef[41:46],
+           function (L) L $ ml.vcv),
+         sample.size = Aux $ sample.size [41:46], 
+         parallel = FALSE, .parallel = TRUE)
+
+runciePost $ diagW.comp <- do.call(arrangeGrob, c(runciePost $ diagW, ncol = 2, nrow = 2))
+ggsave(paste (folder, 'runcie.W.pdf', sep = ''),
+       runciePost $ diagW.comp, width = 16, height = 16)
+
+runciePost $ quantile <-
+  foreach(i = 1:4) %dopar%
+DiagQuantilePop(161, runciePost $ ext [[i]], OneDef, tree = Tree [[5]])
+runciePost $ quantile.comp <-
+  do.call(arrangeGrob, c(runciePost $ quantile, nrow = 4))
+ggsave(paste (folder, 'runcie.quantile.pdf', sep = ''),
+       runciePost $ quantile.comp, width = 16, height = 32)
+
+runciePost $ Drift <- DiagDrift (runciePost $ combine.ext)
+runciePost $ Drift.comp <-
+  do.call(arrangeGrob, c(runciePost $ Drift, 'nrow' = 3))
+ggsave(paste (folder, 'drift.pdf', sep = ''),
+       runciePost $ Drift.comp, width = 12, height = 36)
+
+runciePost $ Drift.mat <- DiagDrift (runciePost $ combine.ext, mode = 'matrix')
+runciePost $ Drift.mat.comp <-
+  do.call(arrangeGrob, c(runciePost $ Drift.mat, 'nrow' = 3))
+ggsave(paste (folder, 'drift.mat.pdf', sep = ''),
+       runciePost $ Drift.mat.comp, width = 12, height = 36)
+
+### vamos olhar para os lambdas
+
+runciePost $ LambdaW.df <- melt (llply (runciePost $ ext, function (L) L $ LambdaW))
+
+runciePost $ LambdaW.plot <- 
+  ggplot (runciePost $ LambdaW.df) +
+  geom_violin(aes (x = trait, y = value, color = L1),
+              alpha = 0.5, scale = 'width') +
+  facet_wrap(~ factor, scales = 'free', ncol = 1) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90))
+
+ggsave(paste (folder, 'LambdaW.pdf', sep = ''),
+       runciePost $ LambdaW.plot, width = 24, height = 48)
+
+runciePost $ LambdaB.df <- melt (llply (runciePost $ ext, function (L) L $ LambdaB))
+
+runciePost $ LambdaB.plot <- 
+  ggplot (runciePost $ LambdaB.df) +
+  geom_violin(aes (x = trait, y = value, color = L1),
+              alpha = 0.5, scale = 'width') +
+  facet_wrap(~ factor, scales = 'free', ncol = 1) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90))
+
+ggsave(paste (folder, 'LambdaB.pdf', sep = ''),
+       runciePost $ LambdaB.plot, width = 24, height = 48)
+
+save(runciePost, file = paste (folder, 'post.RData', sep = ''))
+
+### escrever DiagEvol
+
+## runciePost $ psiW.df <- melt (runciePost $ combine.ext $ PsiW, value.name = 'W')
+## runciePost $ psiB.df <- melt (runciePost $ combine.ext $ PsiB, value.name = 'B')
+
+## runciePost $ psi.df <- cbind (runciePost $ psiW.df,
+##                               runciePost $ psiB.df)
+
+## runciePost $ psi.df <- runciePost $ psi.df[, -(1:2)]
+
+## runciePost $ psibar.df <-
+##   ddply (runciePost $ psi.df, .(trait), summarize, W = mean(W), B = mean(B))
+
+## summary (lm (log(B) ~ log(W), data = runciePost $ psibar.df))
+
+## ggplot (runciePost $ psibar.df) +
+##   geom_text (aes (x = W, y = B, label = trait), alpha = 1, size = 5, shape = '+') +
+##   scale_x_log10() + scale_y_log10() +
+##   ## geom_polygon(aes(x = hullW, y = hullB, color = trait, fill = trait),
+##   ##              data = runciePost $ psi.hull, alpha = 0.4) +
+##   theme_minimal() + geom_smooth(aes(x = W, y = B), formula = y ~ x, method = 'lm')
+
+load (paste (folder, 'post.RData', sep = ''))
+
+Norm.hyp <- t (aaply (Aux $ def.hyp [, 1:6], 2, Normalize))
+
+### Size (easy)
+Norm.hyp <- cbind(c(1, rep (0, 38)), Norm.hyp)
+colnames(Norm.hyp) [1] <- 'Size'
+
+runciePost $ combine.evol.W <- 
+  aaply (runciePost $ combine.ext $ SigmaW, 1,
+         function (C) aaply (Norm.hyp, 2, Evolvability, cov.matrix = C))
+
+runciePost $ combine.evol.B <- 
+  aaply (runciePost $ combine.ext $ SigmaB, 1,
+         function (C) aaply (Norm.hyp, 2, Evolvability, cov.matrix = C))
+
+runciePost $ evol.df <-
+  data.frame (melt(runciePost $ combine.evol.B, value.name = 'B'),
+              melt(runciePost $ combine.evol.W, value.name = 'W'))
+
+runciePost $ evol.df <- runciePost $ evol.df [, -(1:2)] [, c(2, 3, 1, 4)]
+
+colnames (runciePost $ evol.df) <- c('iterations', 'comb', 'B', 'W')
+
+
+### Allometry
+runciePost $ alloW.slope <- aaply (runciePost $ combine.ext $ SigmaW, 1,
+                                  function (C) Normalize (C [, 1] / C[1, 1])
+                                  )
+runciePost $ allo.evol <-
+  adply (1:400, 1, function (i)
+         Evolvability(runciePost $ alloW.slope [i, ],
+                      runciePost $ combine.ext $ SigmaW [i, , ]))
+runciePost $ allo.evol.B <-
+  adply (1:400, 1, function (i)
+         Evolvability(runciePost $ alloW.slope [i, ],
+                      runciePost $ combine.ext $ SigmaB [i, , ]))
+runciePost $ allo.evol.df <-
+  data.frame(1:400,
+    rep('Allo', 400),  
+    runciePost $ allo.evol.B [2],
+    runciePost $ allo.evol [2],
+    stringsAsFactors=FALSE)
+colnames (runciePost $ allo.evol.df) <- c('iterations', 'comb', 'B', 'W')
+
+### PC1
+
+runciePost $ evol.PC1.df <- 
+  data.frame (1:400,
+              rep('PC1', 400),
+              aaply (1:400, 1,
+                     function (i)
+                     c(Evolvability (eigen (runciePost $ combine.ext $ SigmaW [i, , ]) $
+                                     vectors [, 1],
+                                     runciePost $ combine.ext $ SigmaB [i, , ]),
+                       eigen (runciePost $ combine.ext $ SigmaW [i, , ]) $ values [1])))
+
+colnames (runciePost $ evol.PC1.df) <- c('iterations', 'comb', 'B', 'W')
+
+runciePost $ evol.df <- rbind(runciePost $ evol.df, runciePost $ allo.evol.df,
+                              runciePost $ evol.PC1.df)
+
+runciePost $ evol.hull <- ddply (runciePost $ evol.df, .(comb), summarize,
+                                 hullW = W [chull(W, B)],
+                                 hullB = B [chull(W, B)])
+
+ggplot (runciePost $ evol.df) +
+  geom_point (aes (x = W, y = B, color = comb), alpha = 1, size = 5, shape = '+') +
+  scale_x_log10() + scale_y_log10() +
+  geom_polygon(aes(x = hullW, y = hullB, color = comb, fill = comb),
+               data = runciePost $ evol.hull, alpha = 0.4) +
+  theme_minimal() + geom_smooth(aes(x = W, y = B), formula = y ~ x, method = 'lm')
+
+## runciePost $ combine.condevol.W <- 
+##   aaply (runciePost $ combine.ext $ SigmaW, 1,
+##          function (C) aaply (Norm.hyp, 2, ConditionalEvolvability, cov.matrix = C))
+
+## runciePost $ combine.condevol.B <- 
+##   aaply (runciePost $ combine.ext $ SigmaB, 1,
+##          function (C) aaply (Norm.hyp, 2, ConditionalEvolvability, cov.matrix = C))
+
+## runciePost $ condevol.df <-
+##   cbind (melt(runciePost $ combine.condevol.B, value.name = 'B'),
+##          melt(runciePost $ combine.condevol.W, value.name = 'W'))
+
+## runciePost $ condevol.df <- runciePost $ condevol.df [, -(1:2)]
+
+## runciePost $ condevol.hull <- ddply (runciePost $ condevol.df, .(Var2), summarize,
+##                                  hullW = W [chull(W, B)],
+##                                  hullB = B [chull(W, B)])
+
+## ggplot (runciePost $ condevol.df) +
+##   geom_point (aes (x = W, y = B, color = Var2), alpha = 1, size = 5, shape = '+') +
+##   scale_x_log10() + scale_y_log10() +
+##   geom_polygon(aes(x = hullW, y = hullB, color = Var2, fill = Var2),
+##                data = runciePost $ condevol.hull, alpha = 0.4) +
+##   theme_minimal() + geom_smooth(aes(x = W, y = B), formula = y ~ x, method = 'lm')
+
+
---
+++ R/fitDriftFlat.R
@@ -0,0 +1,64 @@
+require (ape)
+require (Morphometrics)
+require (expm)
+require (plyr)
+require (plotrix)
+require (doMC)
+registerDoMC (cores = 4)
+require (reshape2)
+require (ggplot2)
+require (rstan)
+require (phytools)
+require (geiger)
+require (mvtnorm)
+
+attach ('../../Databases/Reference.RData')
+attach ('../../Databases/OneDef/ED.RData')
+attach ('../../Databases/OneDef/OneDef.RData')
+attach ('../../Databases/Tree.RData')
+attach ('../../Databases/Aux.RData')
+attach ('../../covTensor/Work/post.vcv.RData')
+
+.source.files <- dir('../FuncR/', pattern = '.R', full.names = TRUE)
+.source.files <- .source.files [!grepl ('~', .source.files)]
+for (i in 1:length (.source.files))
+  source (.source.files [i])
+
+subtree <- extract.clade (Tree [[5]], 138)
+
+data <- list ()
+data <-
+  within (data,
+          {
+            k <- 39
+            m <- 12 
+            C <- solve(vcvPhylo(subtree))
+            ni <- laply (OneDef [subtree $ tip.label],
+                         function (L) nrow (L $ local))
+            ni_max <- max(ni)
+            X <- array (0, c(m, ni_max, k))
+            for (i in 1:m)
+              X[i, 1:ni[i], ] <- OneDef[[i+26]] $ local
+            n_fac <- 4
+            ###  decomposition
+            a1W <- 2; b1W <- 1 # shrinkageW
+            a2W <- 3; b2W <- 1
+            asW <- 3; bsW <- 1
+            niW <- max(ni)
+          })
+  
+fail.model <-
+  failwith(NULL, function (i)
+           stan('../Stan/oneSigma_Anc_runcie_drift.stan', data = data,
+                pars = c('terminal', 'ancestor', 'root',
+                  'LambdaW', 'PsiW','SigmaW', 'variance_brownian',
+                  'deltaW', 'phiW'),
+                warmup = 1000, iter = 2000, thin = 10, chains = 1,
+                control = list ('chain_id' = i)))
+
+fit <- alply (1:4, 1, fail.model, .parallel = TRUE)
+
+save (data, fit, file = 'driftFlat.RData')
+
+rm (list = ls())
+
